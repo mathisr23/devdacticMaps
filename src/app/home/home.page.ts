bcefdb5fe0592e593bfa34930757af32e14d2cb7
Rivière Mathis <mathis.riviere@edu.devinci.fr>
2021-10-21 18:41:36 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { CapacitorGoogleMaps } from '@capacitor-community/capacitor-googlemaps-native';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,28 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  @ViewChild('map') mapView: ElementRef;
+
   constructor() {}
+
+  ionViewDidEnter() {
+    this.createMap();
+  }
+
+  createMap() {
+    const boundingRect = this.mapView.nativeElement.getBoundingClientRect() as DOMRect;
+    console.log("file: home.page.ts ~ line 21 ~ HomePage ~ createMap ~ boundingRect", boundingRect)
+
+    CapacitorGoogleMaps.create({
+      width: Math.round(boundingRect.width),
+      height: Math.round(boundingRect.height),
+      x: Math.round(boundingRect.x),
+      y: Math.round(boundingRect.y),
+      // latitude?: number;
+      // longitude?: number;
+      zoom: 3,
+      liteMode: false,
+    })
+  }
 
 }
